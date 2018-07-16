189608d2553f6280349133276fe19f0103fddaf7
Oleksandr Vyshnevskyi <mrrgoodcat@gmail.com>
2018-07-16 21:10:27 +0300

    New query modal dialog UI only

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import {CommonModule} from '@angular/common';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 
 import {AppComponent} from './app.component';
 import {DatabaseComponent} from './database/database.component';
@@ -12,11 +15,14 @@ import {NewQueryComponent} from './new-query/new-query.component';
 import {SidebarComponent} from './sidebar/sidebar.component';
 import {DatabasesDialogComponent} from './databases-dialog/databases-dialog.component';
 
+library.add(fas);
 
 import {MCRButtonModule,
         MCRDateRangeModule,
         MCRDialogModule,
         MCRGridModule,
+        MCRLabelModule,
+        MCRTextFieldModule,
         MCRTopBarModule} from 'mcr-components';
 
 @NgModule({
@@ -31,13 +37,16 @@ import {MCRButtonModule,
   imports: [
     CommonModule,
     BrowserAnimationsModule,
+    FontAwesomeModule,
     BrowserModule,
     FormsModule,
     HttpClientModule,
     MCRTopBarModule,
     MCRDialogModule,
+    MCRTextFieldModule,
     MCRGridModule,
     MCRButtonModule,
+    MCRLabelModule,
     MCRDateRangeModule
   ],
   providers: [],
--- src/app/new-query/new-query.component.ts
+++ src/app/new-query/new-query.component.ts
@@ -1,18 +1,31 @@
-import {Component, Input, OnChanges, ViewChild} from '@angular/core';
+import {Component, ElementRef, Input, OnChanges, ViewChild} from '@angular/core';
 import {InteractionsService} from '../shared/services/interactions/interactions.service';
+import {DateRange} from 'mcr-components';
 
 @Component({
   selector: 'pbp-new-query',
   templateUrl: './new-query.component.html',
   styleUrls: ['./new-query.component.scss']
 })
-export class NewQueryComponent {
+export class NewQueryComponent implements OnChanges {
 
-  @ViewChild('dialog') dialog: any;
-  @Input() showDialog: boolean;
-  constructor(interactionsService: InteractionsService) { }
+  @ViewChild('dateRange') dateRangeRef: ElementRef;
 
-  dialogModalConfiguration = {
+  public myModel = {
+    dtRange: {
+      from: new Date(),
+      to: new Date()
+    }
+  };
+
+  dateRangeChanged(dateRange: DateRange) {
+    console.log(this.myModel.dtRange);
+  }
+
+  //@ViewChild('dialog') dialog: any;
+ // @Input() showDialog: boolean;
+
+/*   dialogModalConfiguration = {
     type: 0,
     headerTitle: 'Header',
     message: 'MESSAGE',
@@ -22,15 +35,20 @@ export class NewQueryComponent {
     isCloseOnEscape: true,
     heightOfDialog: 280,
     widthOfDialog: 420
-  };
+  }; */
+  
+  constructor(interactionsService: InteractionsService) {
+    console.log('hhhhhhhhhhhhhh');
+
+   }
 
   addDBQuery(dbId: number, queryName: string, startDate: Date, endDate: Date) {
     this.addDBQuery(dbId, queryName, startDate, endDate);
   }
 
   ngOnChanges() {
-    console.log('-------' + this.showDialog);
-    this.dialog.visible = this.showDialog;
+    /* console.log('-------' + this.showDialog);
+    this.dialog.visible = this.showDialog; */
   }
 
 }
