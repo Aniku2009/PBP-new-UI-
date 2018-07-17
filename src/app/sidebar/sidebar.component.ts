657fac3dec1a3124f233767f5d0425adadd2f070
Oleksandr Vyshnevskyi <mrrgoodcat@gmail.com>
2018-07-17 21:00:09 +0300

    Fix for console

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -2,7 +2,6 @@ import {Component} from '@angular/core';
 import {InteractionsService} from '../shared/services/interactions/interactions.service';
 import {DBState} from '../shared/enums/DBState';
 import {DatabasesDialogComponent} from '../databases-dialog/databases-dialog.component';
-import {GridOptions} from 'ag-grid';
 
 @Component({
   selector: 'pbp-sidebar',
@@ -12,24 +11,7 @@ import {GridOptions} from 'ag-grid';
 export class SidebarComponent {
 
   public DBState = DBState;
-  // gridApi;
-  // gridHeaders = [
-  //   {headerName: 'DB NAME', field: 'Db_name'},
-  //   {headerName: 'DISPLAY NAME', field: 'Display_name'},
-  //   {headerName: 'STATE', field: 'State'},
-  // ];
-  //
-  // gridOptions: GridOptions = {
-  //   domLayout: 'autoHeight',
-  //   rowSelection: 'multiple',
-  //   debug: true,
-  //   onGridReady: this.onGridReady.bind(this),
-  // };
 
   constructor (public interactionsService: InteractionsService) {}
 
-  // private onGridReady (params) {
-  //   this.gridApi = params.api;
-  //   return this.gridApi.sizeColumnsToFit();
-  // }
 }
